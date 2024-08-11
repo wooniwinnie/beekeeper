3f6a0d9c7b053e9d8e44c3e00e240e1e02d7d6d5
wooniwinnie <wooniwinnie@gmail.com>
2024-08-11 18:53:33 +0900

    company 4개 구현

---
+++ dist/company.js
@@ -0,0 +1,181 @@
+"use strict";
+// ABOUT 메뉴를 클릭하면, footer 메뉴의 Company 리스트 내부에
+// About Us, Careers, Press, Blog 페이지들이 존재한다.
+// 동일한 HTML 틀을 제작하여, 각각 서브메뉴들을 클릭했을 때, 페이지들을 다르게
+// 보여주는 코드를 만들어보자, 이 설명을 추후에 수정하도록
+// company container에 About us, Careers, Press, Blog를 넣는다.
+// 타이틀과 서브메뉴 템플릿
+const companyMenu = [
+    {
+        title: 'About Us',
+        submenu: 'About Us',
+        content: {
+            title: 'Home of the Coolest Beekeepers!',
+            text: 'Welcome to the hive of excellence and passion.Where beekeeping meets artistry. Experience the magic of honey made with love. Our beekeepers are the maestros of the bee world. Innovative techniques for the best honey production. Sustainable practices for a healthier planet.Join our community of bee enthusiasts. Learn, grow, and taste the difference. Support beekeepers who care about quality and the environment.Discover the sweetest place on earth!',
+            imgUrl: 'https://plus.unsplash.com/premium_photo-1681506421805-5a72b5528439?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            imgAlt: 'beekeeper man',
+        },
+    },
+    {
+        title: 'Careers',
+        submenu: 'Careers',
+        content: {
+            title: 'We are looking for employees with dreams and passion.',
+            text: "We seek talented individuals to shape the future with us. We offer opportunities to realize your dreams. An environment where passion and creativity can thrive. Innovative projects and challenging tasks. A culture that values teamwork and collaboration. Opportunities for continuous learning and growth. A workplace that respects diversity and inclusion. A fair and transparent hiring process. The chance to work with the world's top experts. Share your talent and passion with us.",
+            imgUrl: 'https://images.unsplash.com/photo-1523287562758-66c7fc58967f?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            imgAlt: 'career',
+        },
+    },
+    {
+        title: 'Press',
+        submenu: 'Press',
+        content: {
+            title: "Ranked Among the World's Top 10 Companies for Contributions to Society",
+            text: 'In a remarkable achievement, XYZ Corporation has been ranked among the world’s top 10 companies for its significant contributions to society. This recognition comes from the Global Corporate Responsibility Index, which evaluates companies based on their social, environmental, and economic impact. XYZ Corporation has long been a leader in sustainable practices, implementing innovative programs that benefit both the environment and local communities. Their commitment to renewable energy, waste reduction, and ethical labor practices has set a high standard in the industry. The company’s CEO, Jane Doe, expressed her pride in the recognition, stating, “This accolade reflects our unwavering dedication to making a positive impact on the world. We will continue to strive for excellence in all aspects of our business. <br>Employees of XYZ Corporation are equally proud, noting that the company’s values align with their personal goals of contributing to a better society. The company’s initiatives, such as the Community Engagement Program and the Green Earth Project, have garnered widespread acclaim and support. As XYZ Corporation moves forward, they plan to expand their efforts, aiming to inspire other companies to follow suit. Their success serves as a powerful reminder that businesses can thrive while also playing a crucial role in societal advancement.',
+            imgUrl: 'https://images.unsplash.com/photo-1504711434969-e33886168f5c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            imgAlt: 'press',
+        },
+    },
+    {
+        title: 'Blog',
+        submenu: 'Blog',
+        content: {
+            title: 'Learning About Bee Characteristics Before Starting Beekeeping',
+            text: 'Before starting beekeeping, understanding the ecology and behavior of bees is essential. Bees have a complex social structure that includes queen bees, worker bees, and drones, each performing distinct roles. Worker bees, in particular, play a crucial role in collecting nectar and pollen. Bees are also vital pollinators, transferring pollen to aid in plant reproduction. A key element for the survival of a bee colony is the regulation of temperature and humidity to optimize the environment within the hive. However, bees can also be defensive, making it important to learn safe beekeeping management practices. Maintaining the health of bees and effectively managing pests and diseases through beekeeping are also critical. This foundational knowledge will greatly assist in successfully starting and managing beekeeping.',
+            imgUrl: 'https://plus.unsplash.com/premium_photo-1681506341082-80c5eabc3558?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            imgAlt: 'traning',
+        },
+    },
+];
+// 만들어진 요소를 삽입하는 함수
+function addElem(parentElem, position, content) {
+    if (parentElem) {
+        parentElem.insertAdjacentHTML(position, content);
+    }
+}
+// 타이틀과 서브메뉴가 삽입될 첫번째 부모요소 선택
+const firstParentElem = document.querySelector('.company__title');
+// 변경할 h1 태그 선택
+const titleItem = document.querySelector('.company__title h1');
+// companyMenu 배열의 서브메뉴를 생성하는 함수를 정의
+const firstParentElemSub = document.querySelector('.company__title__menu');
+function addSubMenu() {
+    // 서브메뉴는 addElem에 전달되어야 하는 콜백함수이므로,
+    // HTML 요소를 값으로 반환하는 함수로 만들어야함
+    // 배열의 모든 서브메뉴들을 순환하여 표시되어야 함
+    let insertSubMenu = '';
+    companyMenu.forEach((subMenu, index) => {
+        let subMenuIndex = index;
+        insertSubMenu += `<li data-index${subMenuIndex}>${subMenu.submenu}</li>`;
+    });
+    // 위에서 생성된 요소들을 반환해주고 함수 실행 종료
+    return insertSubMenu;
+}
+// 우선 클릭이 되어야하는 요소를 선택하기 위해 서브메뉴를 부모요소에 삽입하고 선택자로 지정해야함
+if (firstParentElemSub) {
+    addElem(firstParentElemSub, 'beforeend', addSubMenu());
+}
+// 클릭이 되어야하는 요소들 선택
+const subMenuList = document.querySelectorAll('.company__title__menu li');
+// function addContents() {
+//     let insertContent = `<article><img src="" alt=""></article>
+//         <article><h1>1</h1><p>1</p></article>`;
+//     return insertContent;
+// }
+// HTML 요소들을 삽입할 부모 요소 선택
+const parentElement = document.querySelector('.company__container__content');
+// function resetContentElem(index: number) {
+//     let insetResetContent = '';
+//     let resetContentElemIndex = companyMenu[index];
+//     insetResetContent += `
+//     <article><img src="${resetContentElemIndex.content.imgUrl}" alt="${resetContentElemIndex.content.imgAlt}"></article>
+//       <article><h1>${resetContentElemIndex.content.title}</h1><p>${resetContentElemIndex.content.text}</p></article>
+//     `;
+//     return insetResetContent;
+// }
+if (parentElement) {
+    const resetContent = companyMenu[0].content;
+    parentElement.innerHTML = `
+        <article class="company__container__content__imgbox">
+            <img src="${resetContent.imgUrl}" alt="${resetContent.imgAlt}">
+        </article>
+        <article class="company__container__content__contentbox">
+            <h1>${resetContent.title}</h1>
+            <p>${resetContent.text}</p>
+        </article>
+    `;
+}
+function addContents(index) {
+    const menuItem = companyMenu[index];
+    let insertContent = ``;
+    insertContent += `
+    <article class="company__container__content__imgbox"><img src="${menuItem.content.imgUrl}" alt="${menuItem.content.imgAlt}"></article>
+      <article class="company__container__content__contentbox"><h1>${menuItem.content.title}</h1><p>${menuItem.content.text}</p></article>
+    `;
+    return insertContent;
+}
+// 클릭이 되어지는 요소의 내용을 title 요소의 내용으로 삽입한다.
+subMenuList.forEach((subItem, index) => {
+    subItem.addEventListener('click', (e) => {
+        // 내가 클릭하는 요소의 타겟은 e.target
+        // e.target은 변수에 담아 사용한다.
+        const target = e.target;
+        if (titleItem && target) {
+            if (target instanceof HTMLElement) {
+                titleItem.innerHTML = target.innerText;
+                if (parentElement) {
+                    parentElement.innerHTML = addContents(index);
+                }
+            }
+        }
+    });
+});
+// 문단의 첫 번째 글자의 크기와 색깔을 변경해보자
+// company contents 객체를 만들어서 위에 title 요소 내용 삽입 함수에
+// 콜백함수로 전달한다.
+// if (test) {
+//     test.innerHTML = `<article>
+//     <img/></article>
+//     <article><h1>2</h1><p>2</p></article>
+//     `;
+// }
+// 기본적인 형태
+// Nav 메뉴 ABOUT을 클릭하면 company.html로 이동
+// 좌측 h1 태그는 companyMenu 배열의 title 항목이, 우측 h1태그에는 companyMenu 배열의 subMenu가 표기되어야 함
+// 좌측 h1 태그는 우측 h1태그에서 클릭된 항목과 같아야함
+// subMenuItems.forEach((item, index) => {
+//     item.addEventListener('click', () => {
+//         const index = item.getAttribute('data-index');
+//         if (index !== null) {
+//             const menuIndex = parseInt(index, 10);
+//             if (parentElement && companyMenu[menuIndex]) {
+//                 parentElement.innerHTML = '';
+//                 addElem(
+//                     parentElement,
+//                     'afterbegin',
+//                     addTitle(companyMenu[menuIndex].title)
+//                 );
+//                 addElem(
+//                     parentElement,
+//                     'beforeend',
+//                     companyMenu[menuIndex].content
+//                 );
+//             }
+//         }
+//     });
+// });
+// const listItem = document.querySelector('li');
+// console.log(listItem);
+// const index = listItem?.getAttribute('data-index');
+// console.log(index);
+// listItem?.setAttribute('data-index', '5');
+// console.log(listItem?.getAttribute('data-index'));
+// const exampleDiv = document.getElementById('example');
+// if (exampleDiv) {
+//     exampleDiv.dataset.name = 'Kaeun Lee';
+//     exampleDiv.dataset.age = '25';
+//     exampleDiv.dataset.role = 'designer';
+// }
+// console.log(exampleDiv?.dataset.name);
+// console.log(exampleDiv?.dataset.age);
+// console.log(exampleDiv?.dataset.role);
--- dist/index.js
+++ dist/index.js
@@ -3,21 +3,19 @@
  * 첫인상이 중요하다.
  * 내가 만드는 홈페이지에도 강인한 첫인상을 주고 싶다.
  */
-// const mainDoor = document.querySelector<HTMLElement>('.maindoor');
-// const openTheDoor = document.querySelector<HTMLElement>(
-//     '.maindoor__contents__textbox button'
-// );
-// const mainDoorStyle = { opacity: '0', transition: 'all 1s' };
-// openTheDoor?.addEventListener('click', () => {
-//     if (mainDoor) {
-//         Object.assign(mainDoor.style, mainDoorStyle);
-//         if (mainDoor) {
-//             setTimeout(() => {
-//                 Object.assign(mainDoor.style, { display: 'none' });
-//             }, 500);
-//         }
-//     }
-// });
+const mainDoor = document.querySelector('.maindoor');
+const openTheDoor = document.querySelector('.maindoor__contents__textbox button');
+const mainDoorStyle = { opacity: '0', transition: 'all 1s' };
+openTheDoor?.addEventListener('click', () => {
+    if (mainDoor) {
+        Object.assign(mainDoor.style, mainDoorStyle);
+        if (mainDoor) {
+            setTimeout(() => {
+                Object.assign(mainDoor.style, { display: 'none' });
+            }, 500);
+        }
+    }
+});
 // console.log(mainDoorStyle.opacity === '0');
 // NAVBAR
 // navbar list 메뉴들에 마우스를 올리면 underline이 생긴다.
@@ -79,6 +77,18 @@ class ControlMargin {
             Object.assign(this.elem.style, this.styles);
         }
     }
+    addMarginLeft() {
+        if (this.elem instanceof NodeList) {
+            this.elem.forEach((element) => {
+                Object.assign(element.style, this.styles);
+            });
+        }
+        // 단일 요소라면?
+        // elem이 HTMLElement의 인스턴스라면
+        else if (this.elem instanceof HTMLElement) {
+            Object.assign(this.elem.style, this.styles);
+        }
+    }
 }
 // 단일요소 테스트
 const $img = document.querySelector('.img1');
@@ -89,83 +99,104 @@ if ($img) {
         // $imgControl.addMarginTop();
     });
 }
-/**
- * 비키퍼 이미지 슬라이드 기능 구현하기
- * 상품 이미지 컨테이너에 표현할 수 있는 상품 이미지는 총 세개
- * 가운데 이미지가 양쪽에 있는 이미지보다 클것
- * 상품 이미지에는 그림자 효과가 있을 것
- * 왼쪽, 오른쪽 화살표 버튼이 있고, 그것을 누르면 상품이미지가 양쪽으로 이동한다
- */
-/**
- * 1. HTML 요소 생성
- * 유사 배열 객체처럼 사용하기 위해 공통적인 클래스를 지정한다.
- */
-/**
- * 2.
- */
-// 공부하기
-// const $text = document.querySelectorAll<HTMLElement>('.wrapper p');
-// const $textStyle = { marginRight: '100px' };
-// if ($text) {
-//     const $textControl = new ControlMargin($text, $textStyle);
-//     $text.forEach(($elem) =>
-//         $elem.addEventListener('click', function () {
-//             $textControl.addMarginTop();
-//         })
-//     );
-// }
-// forEach 공부
-// const numbers: object = [1, 2, 3];
-// const pows: object = [];
-// numbers.forEach((num) => {
-//     pows.push(num ** 2);
-// });
-// console.log(pows);
-// [1, 2, 3].forEach((item, index, arr) => {
-//     console.log(
-//         `요소값: ${item} 인덱스: ${index} this: ${JSON.stringify(arr)}`
-//     );
-// });
-// const numbers = [1, 2, 3, 4, 5];
-// numbers.forEach((number, index) => {
-//     console.log(`Index ${index}: ${number}`);
-// });
-// const obj = {
-//     multiplier: 2,
-//     multiply: function (numbers: number) {
-//         numbers.forEach(function (number: number) {
-//             console.log(number * this.multiplier);
-//         }, this);
-//     },
-// };
-// obj.multiply([1, 2, 3, 4]);
-// const numbers = [1, 2, 3, 4, 5];
-// numbers.forEach((number, index) => {
-//     number = number * 2;
-//     console.log(`Index ${index}: ${number}`);
-// });
-// console.log(numbers);
-// const items = [
-//     {
-//         id: 1,
-//         name: 'Item 1',
-//     },
-//     {
-//         id: 2,
-//         name: 'Item 2',
-//     },
-//     {
-//         id: 3,
-//         name: 'Item 3',
-//     },
-// ];
-// items.forEach((item) => {
-//     item.name = 'Updated ' + item.name;
-// });
-// console.log(items);
-// const elements = document.querySelectorAll<HTMLElement>('.element');
-// elements.forEach((element, index) => {
-//     element.style.color = 'blue';
-//     element.style.fontSize = '2rem';
-//     element.textContent = `Updated Element ${index + 1}`;
-// });
+// MAIN SLIDER
+// 이미지 슬라이더를 구현해보자
+// 먼저 단순하게 제작을 해보고, 그 이후에는 페이드 아웃같은 기능도 추가를 해보자
+// 이미지들이 추후에 변경될 가능성이 높고 빈도수가 많으므로
+// HTML 요소들을 객체로 관리하기로 한다.
+const mainSliderContents = [
+    {
+        imgUrl: 'https://images.unsplash.com/photo-1622380997944-c00207483cce?q=80&w=3262&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imgAlt: 'honey bee and flower',
+        title: 'A World with Bees',
+        text: 'The Shortcut to Protecting the Earth',
+        styleName: 'wrapper__img',
+    },
+    {
+        imgUrl: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imgAlt: 'honey',
+        title: 'The BeeKeeper raises bees with love',
+        text: 'As a result, the bees produce healthy honey',
+        styleName: 'wrapper__img',
+    },
+    {
+        imgUrl: 'https://images.unsplash.com/photo-1481900369621-54a7facacc6c?q=80&w=3276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imgAlt: 'healthy honey',
+        title: "The Beekeeper's honey is full of nutrients",
+        text: 'Take care of your health with our honey',
+        styleName: 'wrapper__img',
+    },
+];
+// 콘텐츠 템플릿을 만들어놓자
+// 슬라이더 콘텐츠를 반복시켜서 변수에 할당한다음에
+// 그 변수를 반환시키고 반환시킨 변수를 HTML 삽입 함수 매개변수로 전달함
+const sliderHtml = () => {
+    let insertHtml = '';
+    mainSliderContents.forEach((sliderElem) => (insertHtml += `<article class='${sliderElem.styleName}'>
+    <img src='${sliderElem.imgUrl}' alt='${sliderElem.imgAlt}' />
+    <h1>${sliderElem.title}</h1>
+    <p>${sliderElem.text}</p>
+    </article>`));
+    return insertHtml;
+};
+const mainSliderContainer = document.querySelector('.wrapper');
+mainSliderContainer?.insertAdjacentHTML('afterbegin', sliderHtml());
+const controlSlider = document.querySelectorAll('.wrapper__img');
+const sliderBtn = document.querySelectorAll('.btn');
+//! 이 코드는 좀 더 고민해보고 수정을 진행할 것!!
+sliderBtn.forEach((btnElem, index) => {
+    let count = 0;
+    let btnIndex = index;
+    btnElem.addEventListener('click', (e) => {
+        if (e.target && btnIndex === 1) {
+            count++;
+            console.log('나는 이전버튼', count);
+            if (controlSlider[0]) {
+                controlSlider[0].style.marginLeft = `${count * -100}vw`;
+                if (count >= 3) {
+                    if (controlSlider[0]) {
+                        controlSlider[0].style.marginLeft = '0vw';
+                    }
+                    return (count = 0);
+                }
+            }
+        }
+        if (e.target && btnIndex === 0) {
+            if (controlSlider[0]) {
+                controlSlider[0].style.marginLeft = `${count * 100}vw`;
+            }
+        }
+    });
+});
+// 이미지에 마우스를 오버하면 숨겨져있던 텍스트 박스가 나옴
+// 문제가 생김
+// 이미지 가운데에 마우스를 올려놓고 가만히 있으면
+// 텍스트 박스가 나오지만, 마우스리브 이벤트가 발동되서 디제잉하고 자빠져있음
+// 마우스오버시 동작하는 이벤트 대상
+const imgBox = document.querySelectorAll('.briefintroduction__container__imgbox__img');
+// 마우스오버시 스타일이 변경될 대상
+const textBox = document.querySelectorAll('.briefintroduction__container__textbox');
+// textBox 스타일 객체
+const textBoxStyles = [
+    {
+        transition: 'all 1s',
+        marginLeft: '0px',
+    },
+    {
+        transition: 'all 1s',
+        marginLeft: '-650px',
+    },
+];
+// 마우스오버 이벤트 함수
+imgBox.forEach((imgElem, index) => {
+    imgElem.addEventListener('mouseover', () => {
+        if (textBox[index]) {
+            Object.assign(textBox[index].style, textBoxStyles[0]);
+        }
+    });
+    imgElem.addEventListener('mouseleave', () => {
+        if (textBox[index]) {
+            Object.assign(textBox[index].style, textBoxStyles[1]);
+        }
+    });
+});
